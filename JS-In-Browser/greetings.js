ff3821651df8548e955d14a4b5c5b1e1eb893d7a
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-28 21:36:06 +0530

    JsDay10

--- JS-In-Browser/greetings.js
+++ JS-In-Browser/greetings.js
@@ -6,6 +6,10 @@ export const formalGreeting = (person) => {
     console.log(`Good Morning  ${person}`);
 }
 
+export const casualGreeting = (person) => {
+    console.log(`Howdy ${person}`);
+}
+
 const greeting = () => {
     console.log('Asalamualikum');
 }
