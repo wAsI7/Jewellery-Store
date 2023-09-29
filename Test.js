861e41d75ced49c66bea2e77137565c3b8350f9b
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-29 08:51:14 +0000

    JsDay10

--- Test.js
+++ Test.js
@@ -1,10 +1,30 @@
+//Spread Operator
 const clothes = ['TShirt', 'Shirt', 'Jeans', 'Pants', 'Trousers'];
+const footwear = ['Flip Flops', 34, 'Sneakers', 'Sandals'];
 
-const spreadTest = () => {
-    for(var item of clothes){
+const combineFunc = () => {
+    return [...clothes, ...footwear];
+}
+
+
+var combine = combineFunc(clothes, footwear);
+
+console.log(`Combination of Clothes and Footwear is ${combine}`);
+
+
+const spreadTest = (allItems) => {
+    for(var item of allItems){
         console.log(`${item} is of type ${typeof(item)}`);
     }
 }
 
-spreadTest(clothes);
+spreadTest(combine);
+
+combine.pop();
+
+combine.push('Boots');
+
+console.log(`Combination of Clothes and Footwear is ${combine}`);
+
+
 
