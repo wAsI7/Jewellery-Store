dda89f7048082837e913968fa8616ea0b141f60f
Syed Wasi Ali <wasi.sparkzzz@gmail.com>
2023-09-29 09:38:39 +0000

    JsDay10

--- JS-In-Browser/jsSelectors.js
+++ JS-In-Browser/jsSelectors.js
@@ -6,6 +6,8 @@ console.log(headingText.textContent);
 var paraText = document.getElementsByClassName('myParaTag');
 console.log(paraText.textContent);
 
+var myButton = document.getElementById('button1');
+
 const changeText = () => {
     
 }
