67a6c73798df8445a9858ed9e733a4c2753ed21d
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-02 13:27:54 +0530

    JsDay11

--- JS-In-Browser/webPageContentUpdate.js
+++ JS-In-Browser/webPageContentUpdate.js
@@ -1,3 +1,4 @@
+/*
 let answer = prompt('What is your name?');
 
 if(typeof(answer) === 'string'){
@@ -5,3 +6,19 @@ if(typeof(answer) === 'string'){
     answerHeading.textContent = answer;
     document.body.appendChild(answerHeading);
 }
+*/
+
+var head1 = document.createElement('h1');
+head1.textContent = "Type below to change the Text";
+
+var inputArea = document.createElement('input');
+inputArea.setAttribute('type', 'text');
+
+document.body.innerText = "";
+document.body.appendChild(head1);
+document.body.appendChild(inputArea);
+
+inputArea.addEventListener('change', updateText = () => {
+    console.log(inputArea.value);
+    head1.textContent = inputArea.value;
+})
