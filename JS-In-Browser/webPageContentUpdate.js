2769208409142271af6978591f8f041029b97f1e
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-03 19:53:27 +0530

    JsDay11

--- JS-In-Browser/webPageContentUpdate.js
+++ JS-In-Browser/webPageContentUpdate.js
@@ -23,12 +23,5 @@ inputArea.addEventListener('change', updateText = () => {
     head1.textContent = inputArea.value;
 })
 
-const jsonStr = '{"greetings":"hello"}';
-console.log(`This is JSON ${jsonStr}`);
-
-const jsObj = JSON.parse(jsonStr);
-jsObj.greetings = "Hey!!";
-
-console.log(`This is JS Object ${jsObj}`);
 
 
