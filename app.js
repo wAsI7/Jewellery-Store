bc87aad9333ae9e473572f7abd7be23263db7f55
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-07 00:55:52 +0530

    Jewellery

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ jwtClient.authorize((err) => {
     sheets.spreadsheets.values.get({
         auth: jwtClient,
         spreadsheetId: '1LYmg6F0AtZelZC2RdITRioRGJigqFLMC3saVDzoeV08',
-        range: 'Price' // Specify the sheet name or range
+        range: 'Price'
     }, (err, response) => {
         if (err) {
             console.error('The API returned an error:', err);
