b1f94dc6cf8c39858eb11a9f91c5eab3a3ad9960
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-06 22:01:57 +0530

    JsDay12

--- app.js
+++ app.js
@@ -16,25 +16,25 @@ jwtClient.authorize((err) => {
 }
   // Access your Google Sheet here
 
-sheets.spreadsheets.values.get({
-    auth: jwtClient,
-    spreadsheetId: '0',
-    range: 'Price' // Specify the sheet name or range
-}, (err, response) => {
-    if (err) {
-        console.error('The API returned an error:', err);
-        return;
-    }
-    const values = response.data.values;
-    if (values.length) {
-        console.log('Data from Google Sheet:');
-        values.forEach((row) => {
-            console.log(row);
-        });
-    } else {
-        console.log('No data found in the Google Sheet.');
-    }
-});
+    sheets.spreadsheets.values.get({
+        auth: jwtClient,
+        spreadsheetId: '0',
+        range: 'Price' // Specify the sheet name or range
+    }, (err, response) => {
+        if (err) {
+            console.error('The API returned an error:', err);
+            return;
+        }
+        const values = response.data.values;
+        if (values.length) {
+            console.log('Data from Google Sheet:');
+            values.forEach((row) => {
+                console.log(row);
+            });
+        } else {
+            console.log('No data found in the Google Sheet.');
+        }
+    });
 });
 
 
