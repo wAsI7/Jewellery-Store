6d4ecd70d5bb686864b0bc63f816b99d851d4d7f
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-06 21:36:05 +0530

    JsDay12

--- Test.js
+++ Test.js
@@ -1,6 +1,7 @@
 const jewelleryPrice = (price18Carat) => {
-    var twentyTwoCarat = (916/750) * price18Carat;
-    console.log(twentyTwoCarat);
+    var twentyTwoCarat = 1.221 * price18Carat;
+    var twentyFourCarat = 1.332 * price18Carat;
+    console.log(Math.floor(twentyTwoCarat));
 }
 
 jewelleryPrice(100);
