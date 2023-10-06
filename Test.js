5822f936ee272b2634282d386ac466c84a753290
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-06 21:51:48 +0530

    JsDay12

--- Test.js
+++
@@ -1,9 +0,0 @@
-const twentyTwoCarat = (price18Carat) => {
-    return price = 1.221 * price18Carat;
-}
-
-const twentyFourCarat = (price18Carat) => {
-    return price = 1.332 * price18Carat;
-}
-
-jewelleryPrice(100);
---
+++ script.js
@@ -0,0 +1,8 @@
+const twentyTwoCarat = (price18Carat) => {
+    return price = Math.floor(1.221 * price18Carat);
+}
+
+const twentyFourCarat = (price18Carat) => {
+    return price = Math.floor(1.332 * price18Carat);
+}
+
