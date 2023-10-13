7448c590f5eb4a44aeb1a2dead2b2d883cba8f03
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-13 17:13:28 +0530

    JsDay12

---
+++ Callback.js
@@ -0,0 +1,15 @@
+const fetchData = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const Data = 'Data Fetched';
+        resolve(Data);
+    }, 2000);
+});
+
+fetchData.then((result) => {
+    console.log('Data received:', result);
+}).catch((error) => {
+    console.error('Error:', error);
+});
+
+
+
