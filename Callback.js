f08398dfc58f6134fe912a066ff517677926f04c
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-14 18:07:30 +0530

    JsDay12

--- Callback.js
+++ Callback.js
@@ -11,3 +11,21 @@ fetchData.then((result) => {
 }).catch((error) => {
     console.error('Error:', error);
 });
+
+//Database
+dyetonsolution_dyetondb
+
+//Users
+dyetonsolution_dbuser
+B3$8C0v}M-jL
+
+
+//Website
+dyetonsolution
+vhqkhNxlaCnq%9KPg6
+
+dyetonsolutions@gmail.com
+
+
+nit34ra_in
+
