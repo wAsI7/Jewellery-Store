7088927b110db64bb7c8eebf11aebde494b03829
Syed Wasi Ali <syedwasiali10@gmail.com>
2023-10-20 10:54:51 +0000

    JsDay13

--- Callback.js
+++ Callback.js
@@ -12,20 +12,4 @@ fetchData.then((result) => {
     console.error('Error:', error);
 });
 
-//Exception Handling in JS
-
-try {
-    // Code that might throw an exception
-    let result = x / y;
-    if (isNaN(result)) {
-      throw new Error("Result is not a number");
-    }
-    console.log(result);
-  } catch (error) {
-    // Code to handle the exception
-    console.error("An error occurred:", error.message);
-  } finally {
-    // Code that always runs
-    console.log("Execution complete.");
-  }
-  
+//Strict Mode
